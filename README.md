7554a3b4f5d4cd1403b0909e026cdae1adcc3abb
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-03-12 00:53:14 -0500

    little tweaks

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# weatherDashboard
+# Weather Dashboard
+## Table of Contents
+1. [Description](#Description)
+ 2. [Visual](#Visual)
+ 
+## Description
+ This is your weather dashboard and on this weather dashboard you as a user are able to type in a city in the search bar and have it display the weather for the next 5 days for that city. You are also able to do multiple searches as well.
+
+
+ ## Visual
+ ![A screenshot of one search on the page](./Weather-Dashboard.png)
 
 
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-console.log('file loaded')
-// var formEL = document.querySelector('#city-search');
+// variables created to use in javascript
 var searchBtn = document.getElementById('button');
 var weatherInfo = document.querySelector('#card');
 var forecast = document.querySelector('#section')
 var inputEL = document.querySelector('#search-input');
 var api = '19deadf5a571709d548a2d61112c074d';
 
-
+// three api links that I used to get the city, lat+lon and the 5 day forecast
 var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 var latLongURL = 'https://api.openweathermap.org/data/2.5/weather?lat=';
-
+var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='
+// this was made so when I did a search in the search bar the page would not refresh everytime. I also console logged the data to make sure my button was clicked and working.
 var handleSearchSubmit = function (event) {
   event.preventDefault();
   console.log('ive been clicked');
   console.log(inputEL.value);
-
+// when the user inputs their city the trim will but off any white space and this is also to make sure an actual city was typed in.
   var city = inputEL.value.trim();
   if (city) {
     geoByName(city);
@@ -23,9 +23,9 @@ var handleSearchSubmit = function (event) {
   }
 };
 
-
+// this function was made to collect the data for the city that was input
 var geoByName = function (citysearch) {
-  var citySearchURL = geoURL + citysearch + '&appid=' + api +'&units=imperial';
+  var citySearchURL = geoURL + citysearch + '&appid=' + api + '&units=imperial';
   console.log(citySearchURL);
   fetch(citySearchURL)
     .then(function (response) {
@@ -48,7 +48,7 @@ var display = function (weatherData) {
   }
   var lat = weatherData[0].lat;
   var lon = weatherData[0].lon;
-  var newLatLongURL = latLongURL + lat + '&lon=' + lon + '&appid=' + api +'&units=imperial';
+  var newLatLongURL = latLongURL + lat + '&lon=' + lon + '&appid=' + api + '&units=imperial';
 
   fiveForecast(lat, lon);
 
@@ -81,7 +81,7 @@ var display = function (weatherData) {
       windSpeedEl.textContent = 'Wind Speed: ' + data.wind.speed + ' mph';
       cardEl.appendChild(windSpeedEl);
 
-      
+
 
       weatherInfo.appendChild(cardEl);
 
@@ -90,9 +90,8 @@ var display = function (weatherData) {
 }
 
 
-var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='
 var fiveForecast = function (lat, lon) {
-  var forecastURL = fiveDayURL + lat + '&lon=' + lon + '&appid=' + api +'&units=imperial';
+  var forecastURL = fiveDayURL + lat + '&lon=' + lon + '&appid=' + api + '&units=imperial';
   console.log('hello', forecastURL);
   fetch(forecastURL)
     .then(function (response) {
@@ -100,22 +99,22 @@ var fiveForecast = function (lat, lon) {
         response.json()
           .then(function (data) {
             console.log(data);
-            for(var i = 0; i < 40;  i+=8){
+            for (var i = 0; i < 40; i += 8) {
               var card = document.createElement('div');
               var h3 = document.createElement('h3');
               var temp = document.createElement('p');
               var humidity = document.createElement('p');
               var windSpeed = document.createElement('p');
-  
-  
+
+
               h3.textContent = data.city.name + moment.unix(data.list[i].dt).format(" MM/DD/YYYY");
               console.log(i);
               temp.textContent = 'Temp: ' + data.list[i].main.temp + ' F';
               humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + ' %';
               windSpeed.textContent = 'Wind Speed: ' + data.list[i].wind.speed + ' mph';
-  
-  
-  
+
+
+
               card.appendChild(h3);
               card.appendChild(temp);
               card.appendChild(humidity);
